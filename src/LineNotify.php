2b2b533e70f2cc499db870884b24e6252daff1ad
PITCHAYAKIT-LEG\Pitchayakit <pitchayakit.p@gmail.com>
2022-06-29 09:58:42 +0700

    notify message ability

---
+++ src/LineNotify.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace Pitchayakit\LineNotify;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Support\Facades\Log;
+
+class LineNotify {
+    
+    public static function sendMessage($token, $message)
+    {
+        $client = new Client();
+        $headers = [
+        'Content-Type' => '	application/x-www-form-urlencoded',
+        'Authorization' => "Bearer $token"
+        ];
+        $request = new Request('POST', "https://notify-api.line.me/api/notify?message=$message", $headers);
+        $res = $client->sendAsync($request)->wait();
+
+        Log::debug("=== LineNotify ===");
+        Log::debug($res->getBody());
+
+        return $res->getBody();
+    }
+}
+
+
+?>
